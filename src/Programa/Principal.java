828739af79cb75b8b1c9c5d8252b08c8fb1dcd01
thiagooliveira88 <thiago.saint@hotmail.com>
2018-11-11 22:07:59 -0200

    aaa

--- src/Programa/Principal.java
+++ src/Programa/Principal.java
@@ -53,7 +53,7 @@ public class Principal {
 			inicioExec = System.currentTimeMillis();
 			// faço o processo 5 vezes
 			// HeapSort + Pesquisa binária
-			for (int i = 0; i < 1; i++) {
+			for (int i = 0; i < 4; i++) {
 
 				// leio o arquivo e atribuo o resultado para o vetor de promissoria
 				vetPromissoria = SuporteArquivo.leArquivo(pathArq + arquivo + ".txt", nomeArqs.get(arquivo));
@@ -83,7 +83,7 @@ public class Principal {
 
 			inicioExec = System.currentTimeMillis();
 			// QuickSort + Pesquisa binária
-			for (int i = 0; i < 1; i++) {
+			for (int i = 0; i < 4; i++) {
 
 				// leio o arquivo e atribuo o resultado para o vetor de promissoria
 				vetPromissoria = SuporteArquivo.leArquivo(pathArq + arquivo + ".txt", nomeArqs.get(arquivo));
